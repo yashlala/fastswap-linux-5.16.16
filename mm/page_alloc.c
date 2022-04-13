64e8ccae37168d8b760de3c42c09e07ae1268a59
Yash Lala <yashlala@gmail.com>
2022-04-13 11:26:57 -0700

    fix: implicit header inclusion.
    Might not be necessary.

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -72,6 +72,7 @@
 #include <linux/padata.h>
 #include <linux/khugepaged.h>
 #include <linux/buffer_head.h>
+#include <linux/sched.h>
 #include <asm/sections.h>
 #include <asm/tlbflush.h>
 #include <asm/div64.h>
